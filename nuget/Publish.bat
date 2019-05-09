c03443c48599bd6137d4e00b82c8c18a733b066c
Maud Chiva-Rampazzo <mcr@softfluent.com>
2019-05-09 20:33:57 +0200

    Upgrade of .Net Fx target (4.6.2 to get TLS1.2 by default for remote webservices call) and upgrade of nuget version

--- nuget/Publish.bat
+++ nuget/Publish.bat
@@ -1,2 +1,2 @@
 nuget setApiKey %nugetapikey%
-nuget push SocialEmailLogin.1.0.11.7.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push SocialEmailLogin.1.0.11.8.nupkg -Source https://api.nuget.org/v3/index.json
